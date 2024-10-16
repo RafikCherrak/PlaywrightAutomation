3a91d2b220c90238b5c17a7c8066cfdf6eb63f7f
Rafik Cherrak <cherrakrafik8@gmail.com>
2024-10-16 17:58:20 +0200

    added test for children of frame 3 , handle inner frame

---
+++ tests/HandleInnerFrame.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+
+test("handle inner frame",async ({page})=> {
+ await page.goto("https://ui.vision/demo/webtest/frames")
+
+ const frame3 = await page.frame({url : "https://ui.vision/demo/webtest/frames/frame_3.html"});
+ if(frame3){
+    frame3.locator("input[name='mytext3']").fill("ici c'est paris ")
+    console.log("Test Passed")
+ }else {
+    console.log("Test Failed")
+ }
+
+if(frame3){
+ const childFrame3= await frame3.childFrames()
+
+ if(childFrame3){
+    childFrame3[0].locator("//*[@id='i5']/div[3]/div").check()
+}else {
+    console.log("Test Failed")
+ }
+
+ await page.waitForTimeout(5000);
+}
+})
